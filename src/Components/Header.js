a7e3b341103450a73e0f37ac16051a68ca757167
Katrina Cloyd <Katrina.cloyd@yahoo.com>
2021-03-09 23:46:31 -0800

    header styled and fonts for pages added to app.css

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react'
+import HoverImage from "react-hover-image";
 import { NavLink } from 'react-router-dom'
+import './header.css'
 
 
 export default class Header extends Component {
     render() {
         return (
             <div className='header'>
-                <h1>
-                    Be Head 'Er
-                </h1>
-                <NavLink to='/'>Home</NavLink>
-                <NavLink to='/search'>Search</NavLink>
-                <NavLink to='/my_garden'>My Garden</NavLink>
-                <NavLink to='/wishlist'>Wishlist</NavLink>
-                <NavLink to='/'>Log Out</NavLink>
+                <img className='main-logo' alt='GainfulGarden Logo' src='/GainfulGarden_mainLogo.png' />
+                <div className='link-list'>
+                    <NavLink className='header-link' to='/search'>
+                        <HoverImage className='header-icon' alt='search icon' src='/search_header_icon.png' hoverSrc='/search_icon_Y.png' />
+                        Search
+                    </NavLink>
+                    <NavLink className='header-link' to='/my_garden'>
+                        <HoverImage className='header-icon' alt='search icon' src='/garden_header_icon.png' hoverSrc='/garden_icon_Y.png' />
+                        My Garden
+                    </NavLink>
+                    <NavLink className='header-link' to='/wishlist'>
+                        <HoverImage className='header-icon' alt='search icon' src='/wishlist_header_icon.png' hoverSrc='/wishlist_icon_Y.png' />
+                        Wishlist
+                    </NavLink>
+                    <NavLink className='header-link' to='/'>
+                        <HoverImage className='header-icon' alt='search icon' src='/logout_header_icon.png' hoverSrc='/logout_icon_Y.png' />
+                        Log Out
+                    </NavLink>
+                </div>
             </div>
         )
     }
