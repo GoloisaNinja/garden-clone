5de7aed117b11bd602247852178011481cb32c7e
Kat Sauma <katsauma@gmail.com>
2021-03-09 17:41:43 -0800

    munge functions written

---
+++ ApiUtils.js
@@ -0,0 +1,117 @@
+import request from 'superagent';
+
+const URL = 'http://gainfulgarden.herokuapp.com';
+
+export async function signUpUser(name, email, password) {
+    const response = await request
+        .post(`${URL}/auth/signup`)
+        .send({ name, email, password })
+    return response.body;
+}
+
+export async function loginUser(email, password) {
+    const response = await request
+        .post(`${URL}/auth/signin`)
+        .send({ email, password })
+
+    return response.body;
+}
+
+export async function getNotes(token, id) {
+    const response = await request
+        .get(`${URL}/api/notes/${id}`)
+        .set('Authorization', token)
+
+    return response.body;
+}
+
+export async function getGarden(token) {
+    const response = await request
+        .get(`${URL}/api/my_garden`)
+        .set('Authorization', token)
+
+    return response.body;
+}
+
+export async function getWishlist(token) {
+    const response = await request
+        .get(`${URL}/api/wishlist`)
+        .set('Authorization', token)
+
+    return response.body;
+}
+
+export async function addToWishlist(token, main_species_id) {
+    const response = await request
+        .post(`${URL}/api/wishlist`)
+        .send({ main_species_id })
+        .set('Authorization', token)
+
+    return response.body;
+}
+
+export async function addToGarden(token, main_species_id, plant_name) {
+    const response = await request
+        .post(`${URL}/api/my_garden`)
+        .send({ main_species_id, plant_name })
+        .set('Authorization', token)
+
+    return response.body;
+}
+
+export async function addNotes(token, main_species_id, date, note) {
+    const response = await request
+        .post(`${URL}/api/notes`)
+        .send({ main_species_id, date, note })
+        .set('Authorization', token)
+
+    return response.body;
+}
+
+export async function deleteGardenPlant(token, plantId) {
+    const response = await request
+        .delete(`${URL}/api/my_garden/${plantId}`)
+        .set('Authorization', token)
+
+    return response.body;
+}
+
+export async function deleteWishlistPlant(token, plantId) {
+    const response = await request
+        .delete(`${URL}/api/wishlist/${plantId}`)
+        .set('Authorization', token)
+
+    return response.body;
+}
+
+export async function getEdibles(token) {
+    const response = await request
+        .get(`${URL}/api/edible_search`)
+        .set('Authorization', token)
+
+    return response.body.data;
+}
+
+export async function getPlantDetails(token, main_species_id) {
+    const response = await request
+        .get(`${URL}/api/plant_detail/${main_species_id}`)
+        .set('Authorization', token)
+
+    return response.body.data;
+}
+
+export async function getFilteredSearches(token, part, vegetable, light) {
+    const response = await request
+        .get(`${URL}/api/filtered_search?part=${part}&vegetable=${vegetable}&light=${light}`)
+        .set('Authorization', token)
+
+    return response.body.data;
+}
+
+export async function getNameSearch(token, search_name) {
+    const response = await request
+        .get(`${URL}/api/name_search?name=${search_name}`)
+        .set('Authorization', token)
+
+    return response.body.data;
+}
--- src/Home/Home.js
+++ src/Home/Home.js
@@ -7,6 +7,48 @@ export default class Home extends Component {
                 <h1>
                     There's no place like Home, Auntie Em.
                 </h1>
+                <form className='signup'>
+                    <h2>
+                        New Gardener
+                    </h2>
+                    <label>
+                        Name:
+                        <input
+
+                        />
+                    </label>
+                    <label>
+                        Email:
+                        <input
+
+                        />
+                    </label>
+                    <label>
+                        Password:
+                        <input
+
+                        />
+                    </label>
+                    <button type='submit'>Sign Up</button>
+                </form>
+                <form className='signin'>
+                    <h2>
+                        Established Gardener
+                    </h2>
+                    <label>
+                        Email:
+                        <input
+
+                        />
+                    </label>
+                    <label>
+                        Password:
+                        <input
+
+                        />
+                    </label>
+                    <button type='submit'>Sign In</button>
+                </form>
             </div>
         )
     }
