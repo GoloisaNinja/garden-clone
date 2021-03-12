af4d7c78fa5db70429fc4c69fc12b34d85200fcb
Kat Sauma <katsauma@gmail.com>
2021-03-11 23:37:58 -0500

    About Us styling

--- src/AboutUs/AboutUs.js
+++ src/AboutUs/AboutUs.js
@@ -4,31 +4,31 @@ import "./AboutUs.css"
 export default class AboutUs extends Component {
     render() {
         return (
-            <div>
-                <h1>About the Gainful Team</h1>
+            <div className='about-us-page'>
+                <h1 className='title'>About the Gainful Team</h1>
                 <div className='bioCard'>
-                    <img className='bio-pic-long' alt='Nicole' src='/bio-nicole.png' />
-                    <p>Nicole (she/her) is a full stack software engineer located in Portland, OR. She enjoys working with computers and collaborating on fun and impactful projects that will help the planet, animals, and all humans. Also a huge fan of vegan food, coffee and being outside in the beautiful Pacific Northwest with her dog, Cooper. Her favorite part of gardening is getting to cook and eat all the tasty homegrown veggies!</p>
-                    <a href='https://github.com/nicole-m-martin'>My Github</a>
-                    <a href='https://www.linkedin.com/in/nicolemartinpdx/'>My LinkedIn</a>
+                    <img className='bio-pic' alt='Nicole' src='/bio-nicole.png' />
+                    <p><strong>Nicole (she/her)</strong> <br />is a full stack software engineer located in Portland, OR. She enjoys working with computers and collaborating on fun and impactful projects that will help the planet, animals, and all humans. Also a huge fan of vegan food, coffee and being outside in the beautiful Pacific Northwest with her dog, Cooper. Her favorite part of gardening is getting to cook and eat all the tasty homegrown veggies!</p>
+                    <br /><a href='https://github.com/nicole-m-martin'>My Github</a>
+                    <br /><a href='https://www.linkedin.com/in/nicolemartinpdx/'>My LinkedIn</a>
                 </div>
                 <div className='bioCard'>
                     <img className='bio-pic' alt='Katrina' src='/bio-katrina.jpg' />
-                    <p>Katrina (she/her) is a full stack software engineer in Portland, transplanted from Ohio, and setting growing strong roots. She enjoys coding, design, painting, and gardening. She most recently planted sunflower seeds with her two girls and they have been having a great time watching them sprout!</p>
-                    <a href='https://github.com/KatrinaCloyd?tab=repositories'>My Github</a>
-                    <a href='https://www.linkedin.com/in/katrinacloyd/'>My LinkedIn</a>
+                    <p><strong>Katrina (she/her)</strong> <br /> is a full stack software engineer in Portland, transplanted from Ohio, and setting growing strong roots. She enjoys coding, design, painting, and gardening. She most recently planted sunflower seeds with her two girls and they have been having a great time watching them sprout!</p>
+                    <br /><a href='https://github.com/KatrinaCloyd?tab=repositories'>My Github</a>
+                    <br /><a href='https://www.linkedin.com/in/katrinacloyd/'>My LinkedIn</a>
                 </div>
                 <div className='bioCard'>
                     <img className='bio-pic' alt='KB' src='/bio-kb.jpg' />
-                    <p>KB (she/her) is a software engineer in the city of roses, Portland, OR. She loves to grow beets in her garden and dreams of the day her rosemary plant gets massive!</p>
-                    <a href='https://github.com/katrinkajb'>My Github</a>
-                    <a href='https://www.linkedin.com/in/katy-boyles/'>My LinkedIn</a>
+                    <p><strong>KB (she/her)</strong><br /> is a software engineer in the city of roses, Portland, OR. She loves to grow beets in her garden and dreams of the day her rosemary plant gets massive!</p>
+                    <br /><a href='https://github.com/katrinkajb'>My Github</a>
+                    <br /><a href='https://www.linkedin.com/in/katy-boyles/'>My LinkedIn</a>
                 </div>
                 <div className='bioCard'>
                     <img className='bio-pic' alt='Kat' src='/bio-kat.jpg' />
-                    <p>Kat (they/them) is a dancer and choreographer turned software engineer. They have about 40 plants in their home and enjoy propagating plants for friends. Share the greens!</p>
-                    <a href='https://github.com/kat-sauma'>My Github</a>
-                    <a href='https://www.linkedin.com/in/kat-sauma/'>My LinkedIn</a>
+                    <p><strong>Kat (they/them)</strong><br /> is a dancer and choreographer turned full-stack software engineer. They love combining art and tech and aim to create apps that make being the best citizen possible... a little more possible. They have about 40 plants in their home and enjoy propagating plants for friends. Share the greens!</p>
+                    <br /><a href='https://github.com/kat-sauma'>My Github</a>
+                    <br /><a href='https://www.linkedin.com/in/kat-sauma/'>My LinkedIn</a>
                 </div>
             </div>
         )
